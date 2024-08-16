721c37f01bf39f1d8bb6d171ee9bf2f410a6558b
Syed-TP0146 <syed@testpress.in>
2024-08-16 18:00:14 +0530

    update session details

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
-from .models import Book, Author, BookInstance, Genre
-
 def index(request):
     """View function for home page of site."""
 
@@ -17,11 +14,16 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
