27c35ff88acb30aba3a72cdc9cb5947a9d846904
Syed-TP0146 <syed@testpress.in>
2024-08-19 18:26:15 +0530

    update crud ops for book and author with ui changes

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,3 +13,33 @@ urlpatterns = [
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
+
+from django.urls import path
+from .views import LoanedBooksAllListView
+
+urlpatterns += [
+    path('borrowed/', LoanedBooksAllListView.as_view(), name='all-borrowed'),
+]
+
+# catalog/urls.py
+from django.urls import path
+from .views import BookCreateView, BookUpdateView, BookDeleteView
+
+urlpatterns += [
+    path('book/create/', BookCreateView.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+]
+
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+from django.contrib.auth.decorators import permission_required
+
 
 def index(request):
     """View function for home page of site."""
@@ -60,3 +64,125 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
             .filter(status__exact='o')
             .order_by('due_back')
         )
+import datetime
+
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+
+class AuthorCreate(PermissionRequiredMixin, CreateView): # type: ignore
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'catalog.add_author'
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView): # type: ignore
+    model = Author
+    # Not recommended (potential security issue if more fields added)
+    fields = '__all__'
+    permission_required = 'catalog.change_author'
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView): # type: ignore
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
+        
+from django.views.generic import ListView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from catalog.models import BookInstance
+
+class LoanedBooksAllListView(PermissionRequiredMixin, ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    permission_required = 'catalog.can_mark_returned'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+    
+
+from .models import Book
+from .forms import BookForm
+
+
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'catalog/book_form.html'
+    success_url = reverse_lazy('books')  # Redirect to the list of books after creation
+
+    def form_valid(self, form):
+        if not self.request.user.has_perm('catalog.can_add_book'):
+            return self.handle_no_permission()
+        return super().form_valid(form)
+    
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'catalog/book_form.html'
+    success_url = reverse_lazy('books')  # Redirect to the list of books after editing
+
+    def form_valid(self, form):
+        if not self.request.user.has_perm('catalog.can_change_book'):
+            return self.handle_no_permission()
+        return super().form_valid(form)
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'catalog/book_confirm_delete.html'
+    success_url = reverse_lazy('books')
+
+    def delete(self, request, *args, **kwargs):
+        if not self.request.user.has_perm('catalog.can_delete_book'):
+            return self.handle_no_permission()
+        return super().delete(request, *args, **kwargs)
