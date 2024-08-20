6c2c744ebde33c59d36414090f62d529aa0555c0
Syed Ibrahim <syed@testpress.in>
2024-08-20 15:27:44 +0530

    Update ALLOWED_HOSTS on settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -38,8 +38,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['syedjango.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
